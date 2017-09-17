17c2b6347e695f4ac309c53b0e8fc92a4e46eecf
Unknown <mikeybagnasco@gmail.com>
2017-09-17 17:55:29 -0400

    Setting up classes
    Set up the inheretance on the classes and imported the functions from the ADTs.

--- Bag_Based_Dictionary/Bag_Based_Dictionary/dictionary.h
+++ Bag_Based_Dictionary/Bag_Based_Dictionary/dictionary.h
@@ -6,8 +6,67 @@
 //  Copyright © 2017 Michael Bagnasco. All rights reserved.
 //
 
+#include "dictionaryADT.h"
+#include "bag.h"
+
 #ifndef dictionary_h
 #define dictionary_h
 
+template <typename Key, typename E>
+class  BDictionary: public Dictionary<Key, E> {
+public:
+    BDictionary() {}          // Default constructor
+    ~BDictionary() {} // Base destructor
+    
+    // Reinitialize dictionary
+    void clear()
+    {
+        
+    }
+    
+    // Insert a record
+    // k: The key for the record being inserted.
+    // e: The record being inserted.
+    // Return true if insert is successful and false otherwise
+    bool insert(const Key& k, const E& e)
+    {
+        
+    }
+    
+    // Looks for a record using the key and if found does the following:
+    // - updates the E& rtnVal
+    // - removes the record from the dictionary
+    // - returns true
+    // If the record is not found the function returns false.
+    bool remove(const Key& k, E& rtnVal)
+    {
+        
+    }
+    
+    // Takes an arbitrary record from the dictionary and does the following:
+    // - updates the E& returnValue
+    // - removes the record from the dictionary
+    // - returns true
+    // If the dictionary is empty the function returns false.
+    bool removeAny(E& returnValue)
+    {
+        
+    }
+    
+    // Looks for a record using the key and if found does the following:
+    // - updates the E& returnValue
+    // - returns true
+    // If the record is not found the function returns false.
+    bool find(const Key& k, E& returnValue) const
+    {
+        
+    }
+    
+    // Return the number of records in the dictionary.
+    int size()
+    {
+        
+    }
+};
 
 #endif /* dictionary_h */
--- Bag_Based_Dictionary/Bag_Based_Dictionary/main.cpp
+++ Bag_Based_Dictionary/Bag_Based_Dictionary/main.cpp
@@ -7,9 +7,10 @@
 //
 
 #include <iostream>
+#include "dictionary.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+int main() {
+    
     return 0;
 }
